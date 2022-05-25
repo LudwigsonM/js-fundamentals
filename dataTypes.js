926ff443b69c544cd96518dd7ed80eb974b51ffd
Ludwigson Morisseau <morisseau@Ludwigsons-MacBook-Air.local>
2022-05-24 22:30:52 -0400

    inital commit of my own understanding of JS

---
+++ dataTypes.js
@@ -0,0 +1,11 @@
+// JavaScript has three primative datatypes: number; string; boolean (true|false);
+
+let age = 26; // number
+const firstName = 'Ludwigson' // string
+const black = true;  // boolean
+
+// there are special cases of datatypes
+// null = no value 
+// undefined = not sure of the current value
+// NaN = (Not a Number)
+// Infinity
---
+++ variable.js
@@ -0,0 +1,9 @@
+// variables are assigned with "=" 
+// For a variable to be effective and useful it should
+// be in camelCase; start with a letter; be descriptive; (numbers and _ ok)
+// 'let' variables can change during program execution/running
+// 'const' variables are used to stay consistent during program running.
+
+const firstName = ludwigson
+
+let age = 26            
